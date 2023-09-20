4ec95d05fd510b7d30dd08af8204203da40aac9b
FrancoMuVa <muvafranco@gmail.com>
2023-09-20 13:25:02 +0000

    fix: printing error

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,9 +2,13 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv)
-    c = ":"
+
     if (args - 1 == 0):
-        c = "."
-    print("{} arguments{}".format(args - 1, c))
+        print("{} arguments.".format(args - 1))
+    elif(args - 1 == 1):        
+        print("{} argument:".format(args - 1))
+    else:
+        print("{} arguments:".format(args - 1))
+        
     for ar in range(1, args):
         print("{}: {}".format(ar, sys.argv[ar]))
