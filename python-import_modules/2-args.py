25baec75f7c9af3efbdb6e28844c6ceb3d89d843
FrancoMuVa <muvafranco@gmail.com>
2023-09-20 13:26:22 +0000

    style: pycodestyle fixed

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
 
     if (args - 1 == 0):
         print("{} arguments.".format(args - 1))
-    elif(args - 1 == 1):        
+    elif (args - 1 == 1):
         print("{} argument:".format(args - 1))
     else:
         print("{} arguments:".format(args - 1))
-        
+
     for ar in range(1, args):
         print("{}: {}".format(ar, sys.argv[ar]))
