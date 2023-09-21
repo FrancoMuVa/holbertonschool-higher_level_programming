5b1d206600297df2d66a1dc99fef73c18716ede6
FrancoMuVa <muvafranco@gmail.com>
2023-09-21 00:31:06 +0000

    feat: function that prints a matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    leng_i = len(matrix)
+    for i in range(0, leng_i):
+        leng_j = len(matrix[i])
+        for j in range(0, leng_j):
+            print("{:d}".format(matrix[i][j]), end="")
+        print()
