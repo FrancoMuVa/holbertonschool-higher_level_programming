0f22e14628d0c5dd9b19e6dae3b9b39de303480b
FrancoMuVa <muvafranco@gmail.com>
2023-09-21 00:38:54 +0000

    fix: printing error

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -5,4 +5,6 @@ def print_matrix_integer(matrix=[[]]):
         leng_j = len(matrix[i])
         for j in range(0, leng_j):
             print("{:d}".format(matrix[i][j]), end="")
+            if j != leng_j:
+                print(" ", end="")
         print()
