0e9811bad53ea5f00f7bbd3db6e8043faacf5026
FrancoMuVa <muvafranco@gmail.com>
2023-09-22 06:01:27 +0000

    feat: program that import

---
+++ python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a, b = int(argv[1]), int(argv[3])
+    op = argv[2]
+    if op == "+":
+        resu = a + b
+    elif op == "-":
+        resu = a - b
+    elif op == "*":
+        resu = a * b
+    elif op == "/":
+        resu = a / b
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    print("{} {} {} = {}".format(a, argv[2], b, resu))
