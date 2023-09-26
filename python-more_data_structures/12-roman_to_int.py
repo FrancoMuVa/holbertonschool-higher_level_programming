09123275b37435aa06671f79724a31391195161a
FrancoMuVa <muvafranco@gmail.com>
2023-09-26 23:19:47 +0000

    feat: function that converts a Roman numeral to an integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string:
+        return None
+
+    rom_dic = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+
+    sum = 0
+    for j in range(len(roman_string)):
+        for key, val in rom_dic.items():
+            if j + 1 < len(roman_string):
+                if roman_string[j] == key and val < rom_dic[roman_string[j+1]]:
+                    sum -= val
+                elif roman_string[j] == key:
+                    sum += val
+            else:
+                if roman_string[j] == key:
+                    sum += val
+    return sum
