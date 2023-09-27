e04d64b7067fefe4fb2b06bfe7b8c65e39cc772f
FrancoMuVa <muvafranco@gmail.com>
2023-09-27 12:15:00 +0000

    fix: case: no roman number

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -14,6 +14,10 @@ def roman_to_int(roman_string):
     }
 
     sum = 0
+    for letter in roman_string:
+        if letter not in rom_dic:
+            return sum
+
     for j in range(len(roman_string)):
         for key, val in rom_dic.items():
             if j + 1 < len(roman_string):
