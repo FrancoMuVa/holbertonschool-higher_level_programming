ce8a71122cf84f59a84ea7a690508d8cb1bab00b
FrancoMuVa <muvafranco@gmail.com>
2023-09-28 12:24:50 +0000

    docs: add class documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-class Square(number):
-    """
-        Class that defines a square.
+"""Class that defines a square."""
 
-        Attributes:
-            number (int): number to define its square.
-    """
+
+class Square(number):
+    """Empty Class"""
     pass
