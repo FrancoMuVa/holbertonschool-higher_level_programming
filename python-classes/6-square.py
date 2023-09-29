0bde8bfd3be868c0a4adde78fd00e32b00e294dd
FrancoMuva <muvafranco@gmail.com>
2023-09-29 21:28:26 +0000

    feat: class Square that defines a square by: (based on 5-square.py)

---
+++ python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""Class that defines a square."""
+
+
+class Square:
+    """Square class objet"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        if self.__position[1] == 1:
+            print()
+        for _ in range(self.__size):
+            print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+
+        if self.__size == 0:
+            print()
+
+    @property
+    def position(self):
+        return __position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if not isinstance(value[0], int and value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+        if value[0] < 0 and value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        slef.__position = value
