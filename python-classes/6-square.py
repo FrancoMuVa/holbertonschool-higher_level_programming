a8efa1b31c4b246f1921158fa1637e71d7fca955
FrancoMuVa <muvafranco@gmail.com>
2023-09-30 01:43:17 +0000

    feix: special cases

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,7 +40,7 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        if self.__position[1] == 1:
+        if self.__position[1] == 1 and self.__size != 0 :
             print()
         for _ in range(self.__size):
             print("{}{}".format(" " * self.__position[0], "#" * self.__size))
