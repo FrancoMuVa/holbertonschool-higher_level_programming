0e79622bfa8bad9ea0380077cb81babf2951d46b
FrancoMuVa <muvafranco@gmail.com>
2023-09-30 01:55:53 +0000

    fix: special cases

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -40,10 +40,12 @@ class Square:
         self.__size = value
 
     def my_print(self):
-        if self.__position[1] == 1 and self.__size != 0 :
+        if self.__position[1] == 1:
             print()
-        for _ in range(self.__size):
+        for _ in range(self.__size - 1):
             print("{}{}".format(" " * self.__position[0], "#" * self.__size))
+
+        print("{}{}".format(" " * self.__position[0], "#" * self.__size))
 
         if self.__size == 0:
             print()
