1de3b83ae52bad34d1ba22eda6f2dbe3094cd7ce
FrancoMuVa <muvafranco@gmail.com>
2023-10-01 18:44:24 +0000

    fix: error

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -8,14 +8,12 @@ def add_integer(a, b=98):
     """
         Returns the add of a, b
     """
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-
-    if not isinstance(a, int):
+    if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
-    if not isinstance(b, int):
+    if not isinstance(b, (float, int)):
         raise TypeError("b must be an integer")
+
+    a = int(a)
+    b = int(b)
 
     return (a + b)
