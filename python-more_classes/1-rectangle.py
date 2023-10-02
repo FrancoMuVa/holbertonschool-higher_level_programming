dae65a07079ef1e037636bb0e735116633659776
FrancoMuVa <muvafranco@gmail.com>
2023-10-02 22:08:35 +0000

    fix: private instance attribute

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -16,13 +16,13 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
-        self.__height = height
+        self.height = height
 
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if width < 0:
             raise ValueError("width must be >= 0")
-        self.__width = width
+        self.width = width
 
     @property
     def height(self):
