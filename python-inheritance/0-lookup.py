efe368d4388e973b51a8dc51e74985e27e1dcfec
FrancoMuVa <muvafranco@gmail.com>
2023-10-04 20:42:13 +0000

    docs: add comments

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+    Function that returns a list of all attributes and methods of an objects.
+"""
+
+
 def lookup(obj):
-    """ Returns a list of attributes and methods of an objects """
+    """ Returns a list of attributes and methods of an objects
+
+        Args:
+            obj (object): Return a list of attributes and methods of obj.
+    """
     return dir(obj)
