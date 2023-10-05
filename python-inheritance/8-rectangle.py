e9663d8320cd20d774e52e1859122d9dd356e7e2
FrancoMuVa <muvafranco@gmail.com>
2023-10-05 23:43:31 +0000

    feat: class Rectangle that inherits from BaseGeometry (7-base_geometry.py)

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Class Base Geometry.
+"""
+
+
+class BaseGeometry():
+    """ Base Geometry """
+
+    def area(self):
+        """ Raise a exeption """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Validates value """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+"""
+    class Rectangle that inherits from BaseGeometry.
+"""
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle """
+    def __init__(self, width, height):
+        """ Initialize a Rectangle object """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
