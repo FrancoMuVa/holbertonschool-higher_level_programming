4f4a31c3ce5153129be8957d0e848c65044e01cc
FrancoMuVa <muvafranco@gmail.com>
2023-10-05 11:56:08 +0000

    feat: added class Base Geometry

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    Class Base Geometry.
+"""
+
+
+class BaseGeometry():
+    """ Empty Class """
+    pass
---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Class Base Geometry.
+"""
+
+
+class BaseGeometry():
+    """ Base Geometry """
+
+    def area(self):
+        """ Raise a exeption """
+        raise Exception("area() is not implemented")
