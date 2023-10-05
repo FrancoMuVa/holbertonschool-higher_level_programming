6967f6ddc786bf8edaa22a17e7784d28e0a375e0
FrancoMuVa <muvafranco@gmail.com>
2023-10-05 14:05:20 +0000

    fix: error

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,19 +1,18 @@
-#!/usr/bin/python3
-"""
+""" 
     Class Base Geometry.
 """
 
 
-class BaseGeometry():
+class BaseGeometry:
     """ Base Geometry """
     def area(self):
         """ Raise a exeption """
         raise Exception("area() is not implemented")
-
     def integer_validator(self, name, value):
         """ Validates value """
-        if isinstance(name, str):    
-            if not isinstance(value, int):
-                raise TypeError(f"{name} must be an integer")
-            if value <= 0:
-                raise ValueError(f"{name} must be greater than 0")
+        if isinstance(name, str):
+            self.name = name
+        if not isinstance(value, int):
+            raise TypeError(f"{self.name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{self.name} must be greater than 0")
