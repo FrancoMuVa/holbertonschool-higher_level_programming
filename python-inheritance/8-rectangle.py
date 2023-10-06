f8745ec171c64bfa3436009d07411051f63504de
FrancoMuVa <muvafranco@gmail.com>
2023-10-06 20:10:46 +0000

    fix: width and height are now private

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -30,5 +30,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """ Initialize a Rectangle object """
         super().__init__()
+        self.__width = width
+        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
