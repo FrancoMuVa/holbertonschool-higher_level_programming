4b1512b9833e1c04e21507417585eff480e488a1
FrancoMuVa <muvafranco@gmail.com>
2023-10-06 21:21:57 +0000

    fix: case: no integers in list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -8,4 +8,7 @@ class MyList(list):
     """ MyList class that inherits form list """
     def print_sorted(self):
         """ Prints a sorted list in ascending order """
-        print("{}".format(sorted(self)))
+        if all(isinstance(e, int) for e in self):
+            print(sorted(self))
+        else:
+            raise TypeError("All elements must be integers")
