9142d89dd9fe07565baa5896f573a81ee1aec816
FrancoMuVa <muvafranco@gmail.com>
2023-10-07 01:48:34 +0000

    error

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -32,5 +32,5 @@ class Rectangle(BaseGeometry):
         self.__width = width
         self.__height = height
         super().__init__
-        self.integer_validator("width", self.__width)
-        self.integer_validator("height", self.__height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
