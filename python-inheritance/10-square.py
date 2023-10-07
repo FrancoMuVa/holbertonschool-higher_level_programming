0a92c6fd2f5dc13af1ff464a6ca0bbcbf367079f
FrancoMuVa <muvafranco@gmail.com>
2023-10-07 11:38:29 +0000

    fix: errors

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -53,8 +53,9 @@ class Square(Rectangle):
 
     def __init__(self, size):
         """ Initialize a Squere object """
+        self.integer_validator("size", size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
+        super().__init__(size, size)
 
     def area(self):
         """ Calculate the area of Square """
