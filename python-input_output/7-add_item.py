27a9f457c9d9017803fb878d1e812b68bc51911c
FrancoMuVa <muvafranco@gmail.com>
2023-10-07 23:59:15 +0000

    docs: add documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-from sys import argv
-from os.path import exists
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-
-
 """
     Script that adds all arguments to a Python list,
     and then save them to a file
 """
+from sys import argv
+from os.path import exists
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
 
 if exists("add_item.json"):
