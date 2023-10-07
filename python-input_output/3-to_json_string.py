31c81ca8cb8a84256e83f70a4fac3428497f3ecf
FrancoMuVa <muvafranco@gmail.com>
2023-10-07 20:57:07 +0000

    feat: Function that returns the JSON representation of an object (string)

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import json
+
+"""
+    To json string.
+"""
+
+
+def to_json_string(my_obj):
+    """ Function that returns the JSON representation of an string """
+    return json.dumps(my_obj)
