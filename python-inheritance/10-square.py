170a81861ac5292e82c5a04c21f2350cc35d4e3f
FrancoMuVa <muvafranco@gmail.com>
2023-10-08 04:21:58 +0000

    fix: function

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,56 +1,15 @@
 #!/usr/bin/python3
 """
-    Class Base Geometry.
-"""
-
-
-class BaseGeometry():
-    """ Base Geometry """
-
-    def area(self):
-        """ Raise a exeption """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates value """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""
-    class Rectangle that inherits from BaseGeometry.
-"""
-
-
-class Rectangle(BaseGeometry):
-    """ Rectangle """
-
-    def __init__(self, width, height):
-        """ Initialize a Rectangle object """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
-
-    def area(self):
-        """ Calculate the area """
-        return self.__width * self.__height
-
-    def __str__(self):
-        """ Return a string representation of Rectangle """
-        return f"[Rectangle] { self.__width}/{self.__height}"
-
-
-"""
     Class Square that inherits from Rectangle
 """
 
+Rectangle = __import__("9-rectangle").Rectangle
+
 
 class Square(Rectangle):
-    """ Squere """
-
+    """
+        Squere class
+    """
     def __init__(self, size):
         """
             Initialize a Squere object
@@ -60,5 +19,7 @@ class Square(Rectangle):
         self.__size = size
 
     def area(self):
-        """ Calculate the area of Square """
-        return self.__size * self.__size
+        """
+            Calculate the area of Square
+        """
+        return (self.__size * self.__size)
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,27 +1,8 @@
 #!/usr/bin/python3
 """
-    Class Base Geometry.
-"""
-
-
-class BaseGeometry():
-    """ Base Geometry """
-
-    def area(self):
-        """ Raise a exeption """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Validates value """
-        if type(value) is not int:
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
-
-
-"""
     class Rectangle that inherits from BaseGeometry.
 """
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
@@ -29,10 +10,10 @@ class Rectangle(BaseGeometry):
 
     def __init__(self, width, height):
         """
-            Initialize a Rectangle object 
+            Initialize a Rectangle object
         """
         super().integer_validator("width", width)
         self.__width = width
-        
+
         super().integer_validator("height", height)
         self.__height = height
