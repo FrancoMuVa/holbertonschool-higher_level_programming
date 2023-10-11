c745bf00a353b091f47c969281118265b44449ee
FrancoMuVa <muvafranco@gmail.com>
2023-10-11 16:32:54 +0000

    feat: add gettet and setter for size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,11 +17,26 @@ class Square(Rectangle):
 
             Args:
                 size: (int) The size of a side of the square.
-                x: (int, optional) The x coordinate.
-                y: (int, optional) The y coordinate.
-                id: (int, optional) The ID value.
+                x:    (int, optional) The x coordinate.
+                y:    (int, optional) The y coordinate.
+                id:   (int, optional) The ID value.
         """
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ Getter for the width attribute """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ Setter for the size attribute """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """ Returns a string reprecentation of Square instance """
