a688c7e668baaea0d7ed52269d0cbdfcd04951e2
FrancoMuVa <muvafranco@gmail.com>
2023-11-14 18:28:49 +0000

    fix: VARCHAR NOT NULL

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 -- Script that creates the table unique_id.
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, VARCHAR(256) NOT NULL);
