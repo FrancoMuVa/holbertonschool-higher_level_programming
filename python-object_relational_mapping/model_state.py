1a5b334285043f3fc9b1776d724e184a2e58648f
FrancoMuVa <muvafranco@gmail.com>
2023-11-19 20:12:55 +0000

    docs: added documentation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -11,9 +11,14 @@ Base = declarative_base()
 
 class State(Base):
     """
-
+        Class definition of a State
     """
     __tablename__ = "states"
-    id = Column(Integer, autoincrement=True, unique=True, nullable=True,
-    primary_key=True)
-    name = Column(String(128), nullable=True)
+    id = Column(
+        Integer,
+        autoincrement=True,
+        unique=True,
+        nullable=False,
+        primary_key=True
+    )
+    name = Column(String(128), nullable=False)
