48fee59fc8314038ddfd365f8bd25a8a54be6e52
FrancoMuVa <muvafranco@gmail.com>
2023-11-19 12:10:21 +0000

    feat: Script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+    Script that takes in an argument and displays all values in the
+    states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    conn_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        search=argv[4]
+    )
+    cur = conn_db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = {}\
+    ORDER BY states.id ASC".format(search))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn_db.close()
