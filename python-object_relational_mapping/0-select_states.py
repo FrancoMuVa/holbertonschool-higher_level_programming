eb1c68f2ee49858ba0cd580e2d753b9e9549c918
FrancoMuVa <muvafranco@gmail.com>
2023-11-19 11:14:48 +0000

    fix: fixed errors

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,14 @@ import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
-    conn_db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                            passwd=argv[2], db=argv[3])
-    
+    conn_db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
     cur = conn_db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -17,11 +17,11 @@ if __name__ == '__main__':
         db=argv[3]
     )
     cur = conn_db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'\
-    ORDER BY states.id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row.startswith("N")
+            print(row)
 
     cur.close()
     conn_db.close()
